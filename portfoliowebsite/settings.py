784c8114de9eca39667d67a19e627af9e2b04e2d
marvinmnn <marvinnkom@yahoo.com>
2019-12-06 21:10:41 +0100

    added home page

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'portfoliowebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD':'(0).(0).',
+        'HOST':'localhost',
+        'port':'5432',
     }
 }
 
@@ -121,4 +126,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = '/media/'
+MEDIA_URL = '/media/'
