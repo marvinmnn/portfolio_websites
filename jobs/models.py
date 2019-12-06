59b28b60dbd02123a385c95c49208edeeacdfce2
marvinmnn <marvinnkom@yahoo.com>
2019-12-06 07:33:23 +0100

    added jobs and blog ,then config

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to = 'media')
+    summary = models.CharField(max_length=200)
+
+
+
+
+
+
+
+
+
+
+
